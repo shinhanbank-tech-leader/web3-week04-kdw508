509dcdecc3367d038711ad13d0d93957ee8cd68d
kdw508 <kdw15082@gmail.com>
2026-03-30 14:42:40 +0900

    commit

---
+++ solidity/Contract1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+      bool public a = true;
+      bool public b = false;
+      
+}
---
+++ solidity/Contract2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    
+    uint8 public a = 1;
+    uint16 public b = 300;
+    uint public sum = a+b;
+
+}
